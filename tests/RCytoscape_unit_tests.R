0f9ae69ee07792c387b5c7a0ec89e5c481637bf7
Paul Shannon <pshannon@fhcrc.org>
2012-03-25 22:39:45 +0000

    RCytoscape_unit_tests.R disabled, perhaps redundantly
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RCytoscape@64425 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/RCytoscape_unit_tests.R
+++ tests/RCytoscape_unit_tests.R
@@ -1,2 +1,3 @@
-require("RCytoscape") || stop("unable to load RCytoscape package")
-RCytoscape:::.test()
+print ("Automatic execution of RCytoscape unit tests disabled because they take too long for the build machine.")
+#require("RCytoscape") || stop("unable to load RCytoscape package")
+#RCytoscape:::.test()
